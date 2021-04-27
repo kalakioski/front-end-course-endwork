2d3f72ebda7a80b8c63333110a7b7e0363fbecca
kalakioski <71070273+kalakioski@users.noreply.github.com>
2021-04-27 21:44:54 +0300

    Added font awesome

--- app.js
+++ app.js
@@ -80,3 +80,27 @@ form.addEventListener('submit', (e) => {
     checkRequired([name, email]);
     checkLength(name, 3, 30);
 })
+
+//Image Gallery
+let galleryImages = document.querySelectorAll('.services-cell');
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+galleryImages.forEach(function(image, index){
+    image.onclick = function() {
+
+        getLatestOpenedImg = index + 1;
+
+        let container = document.body;
+        let newImgWindow = document.createElement('div');
+        container.appendChild(newImgWindow);
+        newImgWindow.setAttribute('class', 'img-window');
+        newImgWindow.setAttribute('onclick', 'closeImg()');
+
+        let newImg = image.firstElementChild.cloneNode();
+        newImgWindow.appendChild(newImg);
+        newImg.classList.remove('services-cell_img');
+        newImg.classList.add('popup-img');
+        newImg.setAttribute('id', 'current-img');
+    }
+})
